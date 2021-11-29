411b6e5f253300a4a53dc71a2d31025006bb5375
Chao Zhang <chao.zhang3@nih.gov>
2021-11-29 13:56:25 -0500

    neo4j APIs swagger updated

--- routes/prod.js
+++ routes/prod.js
@@ -3,18 +3,5 @@ module.exports = {
 		host: '127.0.0.1:9200',
 		log: 'error',
 		requestTimeout: 30000
-	},
-	mysql: {
-		connectionLimit: 100,
-		host: 'host.docker.internal',
-		port: 3306,
-		user: 'root',
-		password: '7323560952',
-		db: 'evssip'
-	},
-	neo4j:{
-		NEO4J_DATABASE_USERNAME:"neo4j",
-		NEO4J_DATABASE_PASSWORD:"humidity-initiator-adhesives",
-		NEO4J_DATABASE_URL:"bolt://localhost:7687"
 	}
 };
