27cca4f6349260a339bf46a6c12cf9fd4d6c6263
Francisco M. Miranda <fmanuelos@gmail.com>
2024-01-11 11:21:18 -0500

    Bug Fix: Login User constructor

--- models/neo4j/user.js
+++ models/neo4j/user.js
@@ -1,12 +1,13 @@
 // extracts just the data from the query results
-
-const User = (_node) => {
-  Object.assign(this, _node.properties);
-  if (this.id) { 
-    this.id = this.id.toNumber();
-  } else {
-    this.id = _node.identity.low;
-  };
-};
+class User {
+  constructor(_node) {
+    Object.assign(this, _node.properties);
+    if (this.id) {
+      this.id = this.id.toNumber();
+    } else {
+      this.id = _node.identity.low;
+    }
+  }
+}
 
 export default User;
